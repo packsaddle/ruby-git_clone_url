0fc9c3f3f084e2679de0f4d793f552becbf1409c
sanemat <o.gata.ken@gmail.com>
2015-12-03 23:37:12 +0900

    feat(deps): update uri-ssh_git version
    Breaking change
    
    uri-ssh_git changes behavior.
    
    v1 behavior: #parse always returns path which start with '/', and
    Generic#to_s always returns path's first '/' stripped.
    v2 behavior: #parse returns path which depends on input (absolute path
    or relative path)
    
    git_clone_url uses uri-ssh_git directly, so this changes git_clone_url
    behavior.
    
    https://github.com/packsaddle/ruby-git_clone_url/issues/6
    https://github.com/packsaddle/ruby-uri-ssh_git/issues/14

--- example/simple.rb
+++ example/simple.rb
@@ -8,7 +8,7 @@ https_url_with_userinfo = 'https://user:pass@github.com/schacon/ticgit.git'
 GitCloneUrl.parse(git_url)
 #=> {scheme: 'git', host: 'github.com', path: '/schacon/ticgit.git' }
 GitCloneUrl.parse(ssh_url)
-#=> {scheme: nil, user: 'git', userinfo: 'git', host: 'github.com', path: '/schacon/ticgit.git' }
+#=> {scheme: nil, user: 'git', userinfo: 'git', host: 'github.com', path: 'schacon/ticgit.git' }
 GitCloneUrl.parse(https_url)
 #=> {scheme: 'https', host: 'github.com', path: '/schacon/ticgit.git'}
 GitCloneUrl.parse(https_url_with_userinfo)
